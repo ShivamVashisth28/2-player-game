d823ad56c902b946201a53173e57da1b9d95ae4f
gabriel <gabrielmedinatorres@hotmail.com>
2024-11-10 22:32:25 +0000

    Improvements to button and changes to the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,34 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
   return (
-
-    <div>
-      <div>Smashers</div>
-      <div className="border-black border-2 w-[5em] m-20">
-        <button
-          onClick={() => {
-            navigate("/game");
-          }}
-          className="text-2xl text-red-600"
-        >
+    <div className="principal">
+      <h1>Smashers</h1>
+      <button
+        className="relative cursor-pointer opacity-90 hover:opacity-100 transition-opacity p-[2px] bg-black rounded-[16px] bg-gradient-to-t from-[#8122b0] to-[#dc98fd] active:scale-95"
+        onClick={() => navigate("/game")}
+        id="game-button"
+      >
+        <span className="w-full h-full flex items-center gap-2 px-8 py-3 bg-[#B931FC] text-white rounded-[14px] bg-gradient-to-t from-[#a62ce2] to-[#c045fc]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+          >
+            <path d="M8 13V9m-2 2h4m5-2v.001M18 12v.001m4-.334v5.243a3.09 3.09 0 0 1-5.854 1.382L16 18a3.618 3.618 0 0 0-3.236-2h-1.528c-1.37 0-2.623.774-3.236 2l-.146.292A3.09 3.09 0 0 1 2 16.91v-5.243A6.667 6.667 0 0 1 8.667 5h6.666A6.667 6.667 0 0 1 22 11.667Z"></path>
+          </svg>
           Game
-        
-        </button>
-      </div>
+        </span>
+      </button>
     </div>
   );
 }
