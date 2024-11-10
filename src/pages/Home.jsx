6a5de12df26713bfa03ad6cefe8198f961242639
Prakash Kumar <kumarprakash3909@gmail.com>
2024-11-11 01:05:14 +0530

    fixed btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,12 @@ function Home() {
 
     <div>
       <div>Smashers</div>
-      <div className="border-black border-2 w-14 m-20">
+      <div className="border-black border-2 w-[5em] m-20">
         <button
           onClick={() => {
             navigate("/game");
           }}
+          className="text-2xl text-red-600"
         >
           Game
         
