113451d6f73bdcbae1e934263dcf042522d3dc45
Nguyen Duy Bang <bangb2111971@student.ctu.edu.vn>
2024-11-13 18:52:34 +0700

    Edit Rock Paper Scissor UI, issue #12

--- src/pages/Games/RockPaperGame.jsx
+++ src/pages/Games/RockPaperGame.jsx
@@ -4,10 +4,10 @@ import GameArea from '../../components/GameArea'
 
 function RockPaperGame() {
   return (
-      <div className='bg-red-300 h-screen flex flex-col'>
-        <div className='flex justify-center text-4xl pt-3 font-serif'>Rock Paper Scissor </div>
-        <div className=' rounded-lg h-full m-2 flex items-center justify-around'>
-            <div className='bg-white rounded-lg pt-2 h-[90%] w-[40%]' > 
+      <div className='bg-indigo-700 h-screen font-serif gap-y-0'>     
+        <div className='text-xl	rounded-lg h-full flex flex-col  items-center justify-around gap-y-0'>
+          <h1>Rock Paper Scissor</h1>
+            <div className='bg-white border-4 border-black flex justify-center rounded-lg h-[80%] w-[80%]' > 
                   <GameArea/>
             </div>
           
